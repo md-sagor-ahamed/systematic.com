8f829efe8fd913ecdc41411d12a603f1000dcf05
md-sagor-ahamed <80240720+md-sagor-ahamed@users.noreply.github.com>
2021-03-08 14:16:41 +0600

    Delete Project 6 directory

--- Project 6/js/main.js
+++
@@ -1,8 +0,0 @@
-// BX SLIDER  
-$(document).ready(function(){
-    $('.slider_area').bxSlider();
-    $('#main_menu').meanmenu({
-        meanMenuContainer: '#mobile_menu',
-        meanScreenWidth: 991,
-    });
-  });
